dfc591022021d7ee9de4bfb508d566a124aa4226
mohanliu <mohan@u.northwestern.edu>
2020-12-24 20:29:39 -0600

    migrate from official repo with some customization

---
+++ public/js/commons.js
@@ -0,0 +1,110 @@
+async function requestExternalImage(imageUrl) {
+  const res = await fetch('fetch_external_image', {
+    method: 'post',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({ imageUrl })
+  })
+  if (!(res.status < 400)) {
+    console.error(res.status + ' : ' + await res.text())
+    throw new Error('failed to fetch image from url: ' + imageUrl)
+  }
+
+  let blob
+  try {
+    blob = await res.blob()
+    return await faceapi.bufferToImage(blob)
+  } catch (e) {
+    console.error('received blob:', blob)
+    console.error('error:', e)
+    throw new Error('failed to load image from url: ' + imageUrl)
+  }
+}
+
+function renderNavBar(navbarId, exampleUri) {
+  const examples = [
+    {
+      uri: 'face_detection',
+      name: 'Face Detection'
+    },
+    {
+      uri: 'face_landmark_detection',
+      name: 'Face Landmark Detection'
+    },
+    {
+      uri: 'face_expression_recognition',
+      name: 'Face Expression Recognition'
+    },
+    {
+      uri: 'age_and_gender_recognition',
+      name: 'Age and Gender Recognition'
+    },
+    {
+      uri: 'face_recognition',
+      name: 'Face Recognition'
+    },
+    {
+      uri: 'face_extraction',
+      name: 'Face Extraction'
+    }
+  ]
+
+  const navbar = $(navbarId).get(0)
+  const pageContainer = $('.page-container').get(0)
+
+  const header = document.createElement('h3')
+  header.innerHTML = examples.find(ex => ex.uri === exampleUri).name
+  pageContainer.insertBefore(header, pageContainer.children[0])
+
+  const menuContent = document.createElement('ul')
+  menuContent.id = 'slide-out'
+  menuContent.classList.add('side-nav', 'fixed')
+  navbar.appendChild(menuContent)
+
+  const menuButton = document.createElement('a')
+  menuButton.href = '#'
+  menuButton.classList.add('button-collapse', 'show-on-large')
+  menuButton.setAttribute('data-activates', 'slide-out')
+  const menuButtonIcon = document.createElement('img')
+  menuButtonIcon.src = 'menu_icon.png'
+  menuButton.appendChild(menuButtonIcon)
+  navbar.appendChild(menuButton)
+
+  examples
+    .forEach(ex => {
+      const li = document.createElement('li')
+      if (ex.uri === exampleUri) {
+        li.style.background='#b0b0b0'
+      }
+      const a = document.createElement('a')
+      a.classList.add('waves-effect', 'waves-light', 'pad-sides-sm')
+      a.href = ex.uri
+      const span = document.createElement('span')
+      span.innerHTML = ex.name
+      span.style.whiteSpace = 'nowrap'
+      a.appendChild(span)
+      li.appendChild(a)
+      menuContent.appendChild(li)
+    })
+
+  $('.button-collapse').sideNav({
+    menuWidth: 260
+  })
+}
+
+function renderSelectList(selectListId, onChange, initialValue, renderChildren) {
+  const select = document.createElement('select')
+  $(selectListId).get(0).appendChild(select)
+  renderChildren(select)
+  $(select).val(initialValue)
+  $(select).on('change', (e) => onChange(e.target.value))
+  $(select).material_select()
+}
+
+function renderOption(parent, text, value) {
+  const option = document.createElement('option')
+  option.innerHTML = text
+  option.value = value
+  parent.appendChild(option)
+}
